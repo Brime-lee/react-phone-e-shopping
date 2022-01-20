4d9b23411d16bf3dd767533651f73c6a25c2566c
Ismail Ibrahim <ismail.ibrahimadeiza@gmail.com>
2022-01-20 18:41:57 +0100

    minor changes

--- src/data.js
+++ src/data.js
@@ -106,10 +106,10 @@ export const detailProduct = {
   id: 1,
   title: "Google Pixel - Black",
   img: "img/product-1.png",
-  price: 30,
+  price: 10,
   company: "google",
   info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semmiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semmiotics, DIY coloring book mixtape craft beer sart mmforial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
   inCart: false,
   count: 0,
   total: 0,
